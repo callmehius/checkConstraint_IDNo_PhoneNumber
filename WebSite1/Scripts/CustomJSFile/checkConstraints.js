7f7735beb342a4a5f14411c2f3c68ec4179e0c80
hieunguyen <hieu.n@vlu.edu.vn>
2023-02-02 09:06:17 +0700

    Upload code

---
+++ WebSite1/Scripts/CustomJSFile/checkConstraints.js
@@ -0,0 +1,68 @@
+﻿//Main method
+function checkConstraintsFunction() {
+    const resultCheckIDNo = checkIDNo();
+    const resultCheckPhoneNumber = checkPhoneNumber();
+    if (resultCheckIDNo != "") {
+        document.getElementById('alertLableIdNo').innerHTML = resultCheckIDNo;
+        return false
+    }
+    else if (resultCheckPhoneNumber != "") {
+        document.getElementById('alertPhoneNumber').innerHTML = resultCheckIDNo;
+        return false
+    }
+    else {
+        return true
+    }
+    
+    /*checkPhoneNumber();*/
+}
+//Hàm kiểm tra ràng buộc căn cước công dân 
+function checkIDNo() {
+    //tạo biến câu thông báo
+    var stringAlert = "";
+    //tạo biến rỗng chứa giá trị của CCCD
+    var stringCCCD = "";
+    //gán giá trị CCCD cho biến
+    stringCCCD = document.getElementById('idNoTextBox').value;
+    //tách các ký tự trong chuỗi kiểm tra xem có chứa ký tự kiểu chữ hay không
+    for (var i = 0; i <= stringCCCD.length; i++) {
+        var charCode = stringCCCD.charCodeAt(i);
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            /*document.getElementById('alertLableIdNo').innerHTML = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';*/
+            return stringAlert = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2'
+        }
+    }
+    //Kiểm tra em chuỗi đã đạt 9 hoặc 12 ký tự hay chưa
+    if (stringCCCD.length !== 9 && stringCCCD.length !== 12) {
+        /*ocument.getElementById('alertLableIdNo').innerHTML = 'Vui lòng chỉ nhập 9 hoặc 12 ký tự.';*/
+        return stringAlert = 'Vui lòng chỉ nhập 9 hoặc 12 ký tự.'
+    }
+    else {
+        /*document.getElementById('alertLableIdNo').innerHTML = "";*/
+        return stringAlert=""
+    }
+}
+//Hàm kiểm tra ràng buộc số điện thoại
+function checkPhoneNumber() {
+    //tạo biến rỗng chứa giá trị của số điện thoại
+    var stringPhoneNumber = "";
+    //gán giá trị CCCD cho biến
+    stringPhoneNumber = document.getElementById('phoneNumberTextBox').value;
+    //tách các ký tự trong chuỗi kiểm tra xem có chứa ký tự kiểu chữ hay không
+    for (var i = 0; i <= stringPhoneNumber.length; i++) {
+        var charCode = stringPhoneNumber.charCodeAt(i);
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            document.getElementById('alertPhoneNumber').innerHTML = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';
+            return false
+        }
+    }
+    //Kiểm tra em chuỗi đã đạt 9 hoặc 12 ký tự hay chưa
+    if (stringPhoneNumber.length !== 10) {
+        document.getElementById('alertPhoneNumber').innerHTML = 'Vui lòng chỉ nhập 10 ký tự.';
+        return false
+    }
+    else {
+        document.getElementById('alertPhoneNumber').innerHTML = "";
+        return true
+    }
+}
---
+++ WebSite1/checkConstraintIDNo.js
@@ -0,0 +1,19 @@
+﻿function myFunction() {
+    var stringCCCD = "";
+    stringCCCD = document.getElementById('TextBox1').value;
+    for (var i = 0; i <= stringCCCD.length; i++) {
+        var charCode = stringCCCD.charCodeAt(i);
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            document.getElementById('Label1').innerHTML = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';
+            return false
+        }
+    }
+    if (stringCCCD.length !== 9 && stringCCCD.length !== 12) {
+        document.getElementById('Label1').innerHTML = 'Vui lòng chỉ nhập 9 hoặc 12 ký tự.';
+        return false
+    }
+    else {
+        document.getElementById('Label1').innerHTML = "";
+        return true
+    }
+}
