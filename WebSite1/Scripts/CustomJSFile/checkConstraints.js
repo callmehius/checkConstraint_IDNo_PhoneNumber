66cafa85c1509505459f2ae647cf39103aad0636
callmehius <107375698+callmehius@users.noreply.github.com>
2023-02-02 09:54:47 +0700

    Update checkConstraints.js
    update main method

--- WebSite1/Scripts/CustomJSFile/checkConstraints.js
+++ WebSite1/Scripts/CustomJSFile/checkConstraints.js
@@ -1,20 +1,21 @@
-﻿//Main method
+//Main method
 function checkConstraintsFunction() {
     const resultCheckIDNo = checkIDNo();
     const resultCheckPhoneNumber = checkPhoneNumber();
     if (resultCheckIDNo != "") {
         document.getElementById('alertLableIdNo').innerHTML = resultCheckIDNo;
+        if (resultCheckPhoneNumber != "") {
+            document.getElementById('alertPhoneNumber').innerHTML = resultCheckPhoneNumber;
+        }
         return false
     }
     else if (resultCheckPhoneNumber != "") {
-        document.getElementById('alertPhoneNumber').innerHTML = resultCheckIDNo;
+        document.getElementById('alertPhoneNumber').innerHTML = resultCheckPhoneNumber;
         return false
     }
     else {
         return true
     }
-    
-    /*checkPhoneNumber();*/
 }
 //Hàm kiểm tra ràng buộc căn cước công dân 
 function checkIDNo() {
@@ -28,22 +29,24 @@ function checkIDNo() {
     for (var i = 0; i <= stringCCCD.length; i++) {
         var charCode = stringCCCD.charCodeAt(i);
         if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-            /*document.getElementById('alertLableIdNo').innerHTML = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';*/
             return stringAlert = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2'
         }
     }
     //Kiểm tra em chuỗi đã đạt 9 hoặc 12 ký tự hay chưa
-    if (stringCCCD.length !== 9 && stringCCCD.length !== 12) {
-        /*ocument.getElementById('alertLableIdNo').innerHTML = 'Vui lòng chỉ nhập 9 hoặc 12 ký tự.';*/
+     if (stringCCCD.length == 0) {
+        return stringAlert = 'Không được để trống.'
+    }
+    else if (stringCCCD.length !== 9 && stringCCCD.length !== 12) {
         return stringAlert = 'Vui lòng chỉ nhập 9 hoặc 12 ký tự.'
     }
     else {
-        /*document.getElementById('alertLableIdNo').innerHTML = "";*/
-        return stringAlert=""
+        return stringAlert = ""
     }
 }
 //Hàm kiểm tra ràng buộc số điện thoại
 function checkPhoneNumber() {
+    //tạo biến câu thông báo
+    var stringAlert = "";
     //tạo biến rỗng chứa giá trị của số điện thoại
     var stringPhoneNumber = "";
     //gán giá trị CCCD cho biến
@@ -52,17 +55,17 @@ function checkPhoneNumber() {
     for (var i = 0; i <= stringPhoneNumber.length; i++) {
         var charCode = stringPhoneNumber.charCodeAt(i);
         if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-            document.getElementById('alertPhoneNumber').innerHTML = 'Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';
-            return false
+            return stringAlert ='Vui lòng chỉ nhập dữ liệu kiểu số. Ví dụ: 0,1,2';
         }
     }
     //Kiểm tra em chuỗi đã đạt 9 hoặc 12 ký tự hay chưa
-    if (stringPhoneNumber.length !== 10) {
-        document.getElementById('alertPhoneNumber').innerHTML = 'Vui lòng chỉ nhập 10 ký tự.';
-        return false
+    if (stringPhoneNumber.length == 0) {
+        return stringAlert = 'Không được để trống.'
+    }
+    else if (stringPhoneNumber.length !== 10) {
+        return stringAlert = 'Vui lòng chỉ nhập 10 ký tự.';
     }
     else {
-        document.getElementById('alertPhoneNumber').innerHTML = "";
-        return true
+        return stringAlert = "";
     }
 }
